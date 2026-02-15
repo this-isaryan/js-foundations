a4ca5f813b2aad1b05c45ad0fc20bc5f4af3cfea
this-isaryan <aryankumar11@outlook.com>
2026-02-14 19:08:50 -0500

    JS built-in way added

--- reverse-string.js
+++ reverse-string.js
@@ -1,5 +1,6 @@
 let str = "Hello World";
 let reversedStr = "";
+let reversedJSWay = str.split("").reverse().join("");
 
 for (let i = str.length - 1; i >= 0; i--){
     reversedStr += str[i];
@@ -7,3 +8,4 @@ for (let i = str.length - 1; i >= 0; i--){
 
 console.log(`This is the string "${str}"`);
 console.log(`This is the reversed string "${reversedStr}"`);
+console.log(`This is the JS Way reversed string "${reversedJSWay}"`);
