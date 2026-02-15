931f9612063917b7098b1282d13104b40efa294b
this-isaryan <aryankumar11@outlook.com>
2026-02-14 19:06:36 -0500

    clean output

--- reverse-string.js
+++ reverse-string.js
@@ -5,5 +5,5 @@ for (let i = str.length - 1; i >= 0; i--){
     reversedStr += str[i];
 }
 
-console.log('This is the string  "', str, '"');
-console.log('This is the reversed string "', reversedStr, '"');
+console.log(`This is the string "${str}"`);
+console.log(`This is the reversed string "${reversedStr}"`);
