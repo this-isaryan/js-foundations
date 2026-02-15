68174f72f9f0be8528d769fe908749630cb52c16
this-isaryan <aryankumar11@outlook.com>
2026-02-14 19:01:46 -0500

    Reverse a string

---
+++ reverse-string.js
@@ -0,0 +1,9 @@
+let str = "Hello World";
+let reversedStr = "";
+
+for (let i = str.length - 1; i >= 0; i--){
+    reversedStr += str[i];
+}
+
+console.log('This is the string  "', str, '"');
+console.log('This is the reversed string "', reversedStr, '"');
