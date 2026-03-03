c6d42ede222e16828dd11777eb3ca59dddaeb73c
this-isaryan <aryankumar11@outlook.com>
2026-03-02 20:34:58 -0500

    chore: add package.json with express dependency

---
+++ my-react-app/src/main.jsx
@@ -0,0 +1,19 @@
+import { createRoot } from 'react-dom/client'
+
+const myelement = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+);
+
+createRoot(document.getElementById('root')).render(
+  myelement
+)
