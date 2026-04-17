ab18762346eaf6fcdfd2b1e20b9948ea600db6a2
this-isaryan <aryankumar11@outlook.com>
2026-04-16 20:19:53 -0400

    workout api routes added

--- workout-api/server.js
+++ workout-api/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import workoutRoutes from "./routes/workoutRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -8,5 +9,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/workouts", workoutRoutes);
 
 app.listen(5000, () => console.log('server running'));
