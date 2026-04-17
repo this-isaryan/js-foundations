1bee9c36a23b749ec9ba31274984e7caa9f6cdc6
this-isaryan <aryankumar11@outlook.com>
2026-04-16 20:33:39 -0400

    server updated

--- workout-api/server.js
+++ workout-api/server.js
@@ -12,4 +12,4 @@ app.use(express.json());
 
 app.use("/api/workouts", workoutRoutes);
 
-app.listen(5000, () => console.log('server running'));
+app.listen(8080, () => console.log('server running'));
